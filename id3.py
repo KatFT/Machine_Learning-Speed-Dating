2a5fc21f3e95ead21adfae7f1515424e00e7cdff
Catarina Teixeira <up201805042@fc.up.pt>
2021-05-24 01:24:59 +0100

    id3 com imagem

--- id3.py
+++ id3.py
@@ -74,3 +74,19 @@ predicted_test = clf.predict(dados_teste)
 
 # Model Accuracy, how often is the classifier correct?
 print("Accuracy:",metrics.accuracy_score(resultados_teste, predicted_test))
+
+#desenhar o grafo da arvore de decisao
+from sklearn.tree import export_graphviz
+from six import StringIO  
+from IPython.display import Image  
+import pydotplus
+
+feature_cols = ['id','partner','age','age_o','goal','date','go_out','length','met',	'like',	'prob','match']
+
+dot_data = StringIO()
+export_graphviz(clf, out_file=dot_data,  
+                filled=True, rounded=True,
+                special_characters=True,feature_names = feature_cols,class_names=['0','1'])
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+graph.write_png('id3.png')
+Image(graph.create_png())
