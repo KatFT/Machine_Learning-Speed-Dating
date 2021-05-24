9928e3c9cd5a3953a2b735a2e73493d6478d328f
Catarina Teixeira <up201805042@fc.up.pt>
2021-05-24 19:21:08 +0100

    naive

--- naive.py
+++ naive.py
@@ -5,6 +5,8 @@ import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier # Import Decision Tree Classifier
+#Import Gaussian Naive Bayes model
+from sklearn.naive_bayes import GaussianNB
 
 # load dataset
 pima = pd.read_csv("speedDating_trab.csv")
@@ -45,7 +47,6 @@ pima['age'].fillna(value=gf,inplace=True)
 gf= pima['id'].mode()[0]
 pima['id'].fillna(value=gf,inplace=True)
 
-
 #convertemos de float para int
 pima[['prob','like','met','length','go_out','date','goal','age_o','age','id']]=  pima[['prob','like','met','length','go_out','date','goal','age_o','age','id']].astype(int)
 
@@ -60,31 +61,14 @@ dados = pima.drop(['match'],axis=1)
 # Split dataset into training set and test set (30%/70%)
 dados_treino, dados_teste, resultados_treino, resultados_teste= train_test_split( dados, resultados, test_size=0.3, random_state=1)
 
-# Create Decision Tree classifer object
-clf = DecisionTreeClassifier(criterion="entropy")
 
-# Train Decision Tree Classifer
-clf = clf.fit(dados_treino, resultados_treino)
+#Create a Gaussian Classifier
+gnb = GaussianNB()
+
+#Train the model using the training sets
+gnb.fit(dados_treino,resultados_treino)
 
 #Predict the response for test dataset
-predicted_test = clf.predict(dados_teste)
+predicted = gnb.predict(dados_teste)
+print("Accuracy:",metrics.accuracy_score(resultados_teste, predicted))
 
-
-# Model Accuracy, how often is the classifier correct?
-print("Accuracy:",metrics.accuracy_score(resultados_teste, predicted_test))
-
-#desenhar o grafo da arvore de decisao
-from sklearn.tree import export_graphviz
-from six import StringIO  
-from IPython.display import Image  
-import pydotplus
-
-feature_cols = ['id','partner','age','age_o','goal','date','go_out','length','met',	'like',	'prob','match']
-
-dot_data = StringIO()
-export_graphviz(clf, out_file=dot_data,  
-                filled=True, rounded=True,
-                special_characters=True,feature_names = feature_cols,class_names=['0','1'])
-graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
-graph.write_png('id3.png')
-Image(graph.create_png())
